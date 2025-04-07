9ae18b9d9dc8ec67de47ccf2d934c3dea3472a63
Cheyu0323 <zxzx44585@gmail.com>
2025-04-07 17:29:44 +0800

    feat: add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://cheyu-portfolio.vercel.app', // ← 改成你實際網址
+    generateRobotsTxt: true, // 會自動產生 robots.txt
+};
